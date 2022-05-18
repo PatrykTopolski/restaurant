897a0cbaa9220156a0135ea712751ae8724eaadc
PatrykTopolski <venirro@gmail.com>
2022-05-19 00:30:18 +0200

    added explanation

--- src/main/java/repository/Repository.java
+++ src/main/java/repository/Repository.java
@@ -2,6 +2,8 @@ package repository;
 
 import java.io.*;
 import java.util.List;
+
+//this could be interface for just MenuEntry, but I was trying to recreate CRUD, with little to non success
 public interface Repository<T>{
     void saveAll(List<T> t) throws IOException;
     List<T> readALl() throws IOException;
