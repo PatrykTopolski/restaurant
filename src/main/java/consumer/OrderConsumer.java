235b812008986f91d990693812c87632d3796d97
PatrykTopolski <venirro@gmail.com>
2022-05-18 23:00:09 +0200

    added employee service and get completed orders

--- src/main/java/consumer/OrderConsumer.java
+++ src/main/java/consumer/OrderConsumer.java
@@ -2,9 +2,8 @@ package consumer;
 
 import lombok.RequiredArgsConstructor;
 import model.order.Order;
+import service.EmployeeService;
 import service.OrderService;
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,9 +11,9 @@ import java.util.Optional;
 public class OrderConsumer extends Thread{
     private final OrderService orderService;
     private final List<Order> completedOrders;
+    private final EmployeeService employeeService;
     private Order bench;
     private boolean isRunning = true;
-
     @Override
     public void run() {
         while (isRunning){
@@ -24,13 +23,21 @@ public class OrderConsumer extends Thread{
         }
     }
 
+    public synchronized List<Order> getCompletedOrders(){
+        return completedOrders;
+    }
+
     public void turnOff(){
         isRunning = false;
         System.out.println("turned kitchen off");
     }
 
+    public boolean canBeRun(){
+        return isRunning;
+    }
+
     private void prepareOrder() {
-        int THIRTY_SECONDS =  300;
+        int THIRTY_SECONDS =  30000;
         System.out.println("taking order from bench");
         bench.getOrderedProduct().forEach(x -> {
             try {
