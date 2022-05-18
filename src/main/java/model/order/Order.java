e3a621af935b705a74921b222f754c83a0d2a1b4
PatrykTopolski <venirro@gmail.com>
2022-05-18 22:58:59 +0200

    added explanation

--- src/main/java/model/order/Order.java
+++ src/main/java/model/order/Order.java
@@ -7,6 +7,12 @@ import model.menu.MenuEntry;
 import java.time.Instant;
 import java.util.List;
 
+
+/*
+could be abstract class since order have to be either on spot
+or on delivery, but I wanted to implement builder including this class fields
+using lombok annotations
+ */
 @Data
 @Builder
 public class Order {
