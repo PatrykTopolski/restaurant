9853705b526c10b10c799daaf2213608fe7ce9ad
PatrykTopolski <venirro@gmail.com>
2022-05-19 18:33:33 +0200

    fix cook builder

--- src/main/java/model/employee/Cook.java
+++ src/main/java/model/employee/Cook.java
@@ -9,6 +9,7 @@ import lombok.*;
 public class Cook extends Employee{
     private int cookingTimeReduction;
 
+    @Builder(builderMethodName = "EmployeeBuilder")
     public Cook(int id, String name, String surname, String phone, int cookingTimeReduction) {
         super(id, name, surname, phone);
         this.cookingTimeReduction = cookingTimeReduction;
