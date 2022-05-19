0005e60cda400177ebd934f4af3daa574f3de195
PatrykTopolski <venirro@gmail.com>
2022-05-19 18:32:59 +0200

    fixed problem with not synchronised consumer and service

--- src/main/java/consumer/OrderConsumer.java
+++ src/main/java/consumer/OrderConsumer.java
@@ -1,35 +1,42 @@
 package consumer;
 
 import lombok.RequiredArgsConstructor;
+import model.employee.Cook;
+import model.order.DeliveryOrder;
 import model.order.Order;
+import model.order.SpotOrder;
 import service.EmployeeService;
 import service.OrderService;
-import java.util.List;
+import utils.TimeMillisecondsConstant;
+
 import java.util.Optional;
+import java.util.Queue;
 import java.util.Random;
 
 @RequiredArgsConstructor
 public class OrderConsumer extends Thread{
     private final OrderService orderService;
-    private final List<Order> completedOrders;
+    private final Queue<Order> completedOrders;
     private final EmployeeService employeeService;
     private Order bench;
-    private boolean isRunning = true;
+    //volatile forces atomic operations (kind of like synch)
+    private volatile boolean isRunning = true;
     private Random random = new Random();
+
     @Override
     public void run() {
-        while (isRunning){
+        while (isRunning ){
             loadOrderToBench();
             if (bench!= null)
                 prepareOrder();
         }
     }
 
-    public synchronized List<Order> getCompletedOrders(){
+    public synchronized Queue<Order> getCompletedOrders(){
         return completedOrders;
     }
 
-    public void turnOff(){
+    public void killThread(){
         isRunning = false;
         System.out.println("turned kitchen off");
     }
@@ -38,20 +45,66 @@ public class OrderConsumer extends Thread{
         return isRunning;
     }
 
+    public Optional<Order> getNextDeliver(){
+        synchronized (completedOrders){
+            Optional<Order> order = completedOrders.stream().filter(x -> x instanceof DeliveryOrder).findFirst();
+            if (!order.isPresent()){
+                try {
+                    System.out.println("no next order to DELIVER");
+                    completedOrders.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            else
+                completedOrders.remove(order.get());
+            return order;
+        }
+    }
+
+    public Optional<Order> getNextSpot(){
+        synchronized (completedOrders){
+            Optional<Order> order = completedOrders.stream().filter(x -> x instanceof SpotOrder).findFirst();
+            if (!order.isPresent()){
+                try {
+                    System.out.println("no next order to DELIVER on spot");
+                    completedOrders.wait();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    // handle the interrupt
+                    return Optional.empty();
+                }
+            }
+            else
+                completedOrders.remove(order.get());
+            return order;
+        }
+    }
+
     private void prepareOrder() {
-        int THIRTY_SECONDS =  30000;
+        int milisecounds = TimeMillisecondsConstant.THIRTY_SECONDS;
+        for (Cook cook: employeeService.getCooks()
+             ) {
+            milisecounds-= cook.getCookingTimeReduction();
+        }
+        System.out.println("time reduced in milis: " + milisecounds);
+        int finalTime = milisecounds;
         System.out.println("taking order from bench");
         bench.getOrderedProduct().forEach(x -> {
             try {
                 System.out.println("producing entry from order");
-                Thread.sleep(THIRTY_SECONDS);
+                Thread.sleep(finalTime);
             } catch (InterruptedException e) {
-                e.printStackTrace();
+                Thread.currentThread().interrupt();
+                System.out.println("interrupting");
             }
         });
         System.out.println("finished cooing order ");
-        completedOrders.add(bench);
-        System.out.println("added order to finished");
+        synchronized (completedOrders) {
+            completedOrders.add(bench);
+            completedOrders.notifyAll();
+        }
+        System.out.println("added order to finished and notified suppliers");
     }
 
 
@@ -71,12 +124,19 @@ public class OrderConsumer extends Thread{
             }
 
         }else {
-            Optional<Order> nextOrder = orderService.getNextOrder();
+            Optional<Order> nextOrder;
+            nextOrder = orderService.getNextOrder();
             bench = nextOrder.orElse(null);
         }
     }
 
     private void applyDiscount(Order order){
         order.getOrderedProduct().forEach(product -> product.setPrice(product.getPrice() * 0.8));
+    }
+
+    public void wakeToKill(){
+        synchronized (completedOrders){
+            completedOrders.notifyAll();
+        }
     }
 }
--- src/main/java/service/OrderService.java
+++ src/main/java/service/OrderService.java
@@ -2,11 +2,13 @@ package service;
 
 import lombok.RequiredArgsConstructor;
 import model.order.Order;
+import utils.TimeMillisecondsConstant;
 
 import java.time.Duration;
 import java.time.Instant;
 import java.util.Optional;
 import java.util.PriorityQueue;
+
 
 @RequiredArgsConstructor
 public class OrderService {
@@ -14,24 +16,40 @@ public class OrderService {
 
     private final PriorityQueue<Order> orderQueue;
 
-    public synchronized void addOrder(Order order){
-        order.setId(orderNumber++);
-        orderQueue.add(order);
+
+    public  void addOrder(Order order){
+        synchronized (orderQueue) {
+            order.setId(orderNumber++);
+            orderQueue.add(order);
+            orderQueue.notify();
+            System.out.println("notified");
+        }
     }
 
     public synchronized void printOrdersInQueue(){
         orderQueue.forEach(System.out::println);
     }
 
-    public synchronized Optional<Order> getNextOrder(){
-        return Optional.ofNullable(orderQueue.poll());
+    public Optional<Order> getNextOrder(){
+        synchronized (orderQueue) {
+            if(orderQueue.isEmpty()){
+                try {
+                    System.out.println("waiting for order");
+                    orderQueue.wait();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    // handle the interrupt
+                    return Optional.empty();
+                }
+            }
+            return Optional.ofNullable(orderQueue.poll());
+        }
     }
 
     public synchronized Optional<Order> getNextDelayedOrder(){
         Optional<Order> overdueOrder = orderQueue.stream().filter(order -> {
-            long FIFTEEN_MINUTES = 900000;
             Duration durationBetween = Duration.between(order.getOrderTime(), Instant.now());
-            return durationBetween.toMillis() > FIFTEEN_MINUTES;
+            return durationBetween.toMillis() > TimeMillisecondsConstant.FIFTEEN_MINUTES;
         }).findFirst();
         overdueOrder.ifPresent(orderQueue::remove);
         return overdueOrder;
@@ -39,6 +57,14 @@ public class OrderService {
 
     public synchronized void delete(Order order){
         orderQueue.remove(order);
+    }
+
+
+    //should use interrupted to kill thread, but I have just one thread so its easier
+    public void wakeToKill(){
+        synchronized (orderQueue){
+            orderQueue.notifyAll();
+        }
     }
 
 }
