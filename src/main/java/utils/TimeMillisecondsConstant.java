c274f9dfa2fcfdff459eba43dada8ad7ffa343f6
PatrykTopolski <venirro@gmail.com>
2022-05-19 18:31:21 +0200

    added time utils

---
+++ src/main/java/utils/TimeMillisecondsConstant.java
@@ -0,0 +1,9 @@
+package utils;
+
+public class TimeMillisecondsConstant {
+
+    public static final int THIRTY_SECONDS = 30000;
+    public static final int TWO_MINUTES = 5; //120000
+    public static final int TEN_SECONDS = 5; //10000
+    public static final int FIFTEEN_MINUTES = 900000;
+}
