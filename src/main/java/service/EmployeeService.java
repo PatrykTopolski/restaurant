c3af79c97a9a22770a3f3688012dba91d47c0b06
PatrykTopolski <venirro@gmail.com>
2022-05-19 18:33:25 +0200

    add employee service

--- src/main/java/service/EmployeeService.java
+++ src/main/java/service/EmployeeService.java
@@ -1,5 +1,6 @@
 package service;
 
+import exceptions.LastEmployeeException;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import model.employee.Cook;
@@ -12,24 +13,118 @@ import java.util.Optional;
 @Getter
 public class EmployeeService {
     private final List<Cook> cooks;
-    private final List<Supplier> suppliers;
+    private final List<Supplier> deliverySuppliers;
+    private final List<Supplier> waiterSuppliers;
+    private volatile int suppliersNumber = 0;
+    private volatile int cooksNumber = 0;
 
-    public void addCook(Cook cook){
+
+    public synchronized void addSupplier(Supplier supplier){
+        supplier.setId(suppliersNumber++);
+        System.out.println("Added new supplier");
+        if (supplier.getType().equals(Supplier.Type.DELIVERY_MAN)){
+            synchronized (deliverySuppliers) {
+                deliverySuppliers.add(supplier);
+                deliverySuppliers.notify();
+            }
+
+        }else {
+            synchronized (waiterSuppliers) {
+                waiterSuppliers.add(supplier);
+                waiterSuppliers.notify();
+            }
+        }
+    }
+
+    public synchronized void addCook(Cook cook){
+        cook.setId(cooksNumber++);
         System.out.println("Added new cook");
         cooks.add(cook);
     }
 
-    private void deleteSupplierById(int id){
-        Optional<Supplier> supplier = suppliers.stream().filter(x-> x.getId() == id).findFirst();
-        if (supplier.isPresent()){
-            suppliers.remove(supplier.get());
-        }else System.out.println("no such employee");
+
+    public Optional<Supplier> takeNextDeliverySupplier(){
+        synchronized (deliverySuppliers) {
+            Optional<Supplier> sup = deliverySuppliers.stream()
+                    .filter(x-> !x.isLocked())
+                    .findFirst();
+            if (!sup.isPresent()) {
+                try {
+                    System.out.println("all delivery suppliers are busy");
+                    deliverySuppliers.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            return sup;
+        }
     }
 
-    private void deleteCookById(int id){
+    public Optional<Supplier> takeNextWaiterSupplier(){
+        synchronized (waiterSuppliers) {
+            Optional<Supplier> sup = waiterSuppliers.stream()
+                    .filter(x-> !x.isLocked())
+                    .findFirst();
+            if (!sup.isPresent()) {
+                try {
+                    System.out.println("all suppliers are busy");
+                    waiterSuppliers.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            return sup;
+        }
+    }
+
+    public synchronized void deleteSupplierById(int id){
+        Optional<Supplier> supplier = deliverySuppliers.stream().filter(x-> x.getId() == id).findFirst();
+        if (supplier.isPresent()){
+            deliverySuppliers.remove(supplier.get());
+            System.out.println("supplier deleted");
+            if (deliverySuppliers.size()==0){
+                throw new LastEmployeeException();
+            }
+        }else {
+            Optional<Supplier> waiterSupp = waiterSuppliers.stream().filter(x-> x.getId() == id).findFirst();
+            if (waiterSupp.isPresent()){
+                waiterSuppliers.remove(waiterSupp.get());
+                System.out.println("waiter deleted");
+                if (waiterSuppliers.size()==0){
+                    throw new LastEmployeeException();
+                }
+            }else
+                System.out.println("no such supplier");
+        }
+    }
+
+    public synchronized void deleteCookById(int id){
+
         Optional<Cook> cook = cooks.stream().filter(x-> x.getId() == id).findFirst();
         if (cook.isPresent()){
             cooks.remove(cook.get());
+            if (cooks.size() == 0){
+                throw new LastEmployeeException();
+            }
         }else System.out.println("no such employee");
+    }
+
+    public synchronized void printEmployees(){
+        System.out.println("cooks");
+        cooks.forEach(System.out::println);
+        System.out.println("waiter suppliers");
+        waiterSuppliers.forEach(System.out::println);
+        System.out.println("deliveryman suppliers");
+        deliverySuppliers.forEach(System.out::println);
+
+    }
+
+    public void wakeToKill(){
+        synchronized (deliverySuppliers){
+            deliverySuppliers.notifyAll();
+        }
+        synchronized (waiterSuppliers){
+            waiterSuppliers.notifyAll();
+        }
     }
 }
